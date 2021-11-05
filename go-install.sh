8c20a8b68ad97165f79776bb41d383edab90881a
dayan <eam.dayan@gmail.com>
2021-11-05 18:07:51 +0000

    fix go install script

--- go-install.sh
+++ go-install.sh
@@ -7,10 +7,11 @@ if ! command -v go &> /dev/null
 then 
 	curl -O https://dl.google.com/go/go1.17.2.linux-amd64.tar.gz
 	sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.17.2.linux-amd64.tar.gz
-	rm go1.17.2.linux-amd64.tar.gz
+	#rm go1.17.2.linux-amd64.tar.gz
 
 	echo "export GOPATH=\$HOME/go" >> ~/.bashrc
 	echo "export PATH=\$PATH:/usr/local/go/bin:\$GOPATH/bin" >> ~/.bashrc
+	source ~/.bashrc
 	eval "$(cat ~/.bashrc | tail -n +10)"
 	exit
 else 
