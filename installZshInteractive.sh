b47ac82970afd87c18638e00c3bef105105b595f
Dayan Eam <77480452+saint-rivers@users.noreply.github.com>
2022-08-03 12:53:59 +0700

    Update installZshInteractive.sh

--- installZshInteractive.sh
+++ installZshInteractive.sh
@@ -8,3 +8,7 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 
 echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
+
+chsh -s $(which zsh)
+
+echo "NOTE: Please logout and log in again for the default shell to be set correctly"
